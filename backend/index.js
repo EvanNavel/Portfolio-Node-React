8cbdc1e64dc0533d2369ffc71cd29936d872c139
Robert Evan Patterson <mechapoltergeist333@gmail.com>
2025-02-06 17:35:44 -0600

    small fixes

--- backend/index.js
+++ backend/index.js
@@ -45,5 +45,5 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
